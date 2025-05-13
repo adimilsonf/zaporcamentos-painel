6de8c4df1c592472236686f85a32df2b9b80c916
adimilsonf <149102076+adimilsonf@users.noreply.github.com>
2025-05-13 16:56:36 -0300

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -63,7 +63,10 @@ export default function Dashboard() {
               </button>
             </div>
           )}
-          <Link to="/perfil" className="text-sm text-gray-700 underline hover:text-blue-600">
+          <Link
+            to="/perfil"
+            className="bg-gray-300 text-gray-800 px-4 py-2 rounded hover:bg-gray-400 text-sm"
+          >
             Ver Perfil
           </Link>
         </div>
