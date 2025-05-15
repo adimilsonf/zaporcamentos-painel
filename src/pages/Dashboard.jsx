7652024ade583cb3462622422e62d30b4ae4f601
adimilsonf <149102076+adimilsonf@users.noreply.github.com>
2025-05-15 14:52:14 -0300

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
-import logo from '../assets/logo.png'; // ✅ Importa a logo
+import logo from '../assets/menu.png'; // ✅ Importa a logo
 
 export default function Dashboard() {
   const [orcamentos, setOrcamentos] = useState([]);
